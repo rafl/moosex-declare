50bc89cec15d05f30e280a97cea43b80cd0e31e7
Florian Ragwitz <rafl@debian.org>
2009-02-21 22:46:07 +0100

    Version 0.04.

--- lib/MooseX/Declare.pm
+++ lib/MooseX/Declare.pm
@@ -8,7 +8,7 @@ use Moose::Meta::Class;
 use B::Hooks::EndOfScope;
 use MooseX::Method::Signatures;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 our ($Declarator, $Offset, %Outer_Stack, @Roles);
 
