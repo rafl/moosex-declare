0f41981d0e8fefa2b9a14e2f0b905b80e2b949a1
Ash Berlin <ash_github@firemirror.com>
2009-03-22 07:53:48 +0800

    Make keywords less reversed (i.e. as hash keys) and make class(); return an anon-metaclass
    Signed-off-by: Nelo Onyiah <nelo.onyiah@gmail.com>

--- lib/MooseX/Declare/Context.pm
+++ lib/MooseX/Declare/Context.pm
@@ -2,44 +2,57 @@ package MooseX::Declare::Context;
 
 use strict;
 use warnings;
+use Carp qw/croak/;
 
 use base 'Devel::Declare::Context::Simple';
 
-sub strip_options {
+sub strip_name_and_options {
     my ($self) = @_;
     $self->skipspace;
 
-    my %ret;
+    # Make errors get reported from right place in source file
+    local $Carp::Internal{'MooseX::Declare'} = 1;
+    local $Carp::Internal{'Devel::Declare'} = 1;
+
+    my ($name, %ret);
     my $linestr = $self->get_linestr();
 
-    while (substr($linestr, $self->offset, 1) ne '{') {
+    while (substr($linestr, $self->offset, 1) !~ /[{;]/) {
         my $key = $self->strip_name;
         if (!defined $key) {
-            die 'expected option name';
+
+            croak 'expected option name'
+              if keys %ret;
+            return; # This is the case when { class => 'foo' } happens
         }
 
         if ($key !~ /^(extends|with|is)$/) {
-            die "unknown option name '${key}'";
+            unless (keys %ret) {
+              $name = $key;
+              $self->skipspace;
+              $linestr = $self->get_linestr();
+              next;
+            }
+            croak "unknown option name '$key'";
         }
 
         my $val = $self->strip_name;
         if (!defined $val) {
-            die 'expected option value';
+            croak "expected option value after $key";
         }
 
         $ret{$key} ||= [];
         push @{ $ret{$key} }, $val;
         $self->skipspace;
         $linestr = $self->get_linestr();
-
     }
 
-    return { map {
+    return ($name, { map {
         my $key = $_;
         $key eq 'is'
             ? ($key => { map { ($_ => 1) } @{ $ret{$key} } })
             : ($key => $ret{$key})
-    } keys %ret };
+    } keys %ret } );
 }
 
 
