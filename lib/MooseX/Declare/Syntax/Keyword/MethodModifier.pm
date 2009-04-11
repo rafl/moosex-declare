eb6f2653e8359ae3a850e5e6d6d88cacc9ec7678
Robert 'phaylon' Sedlacek <rs@474.at>
2009-04-11 03:48:26 +0200

    method declaration splitup

--- lib/MooseX/Declare/Syntax/Keyword/MethodModifier.pm
+++ lib/MooseX/Declare/Syntax/Keyword/MethodModifier.pm
@@ -1,50 +1,22 @@
 package MooseX::Declare::Syntax::Keyword::MethodModifier;
 
 use Moose;
-
-use MooseX::Method::Signatures::Meta::Method;
+use Moose::Util;
+use Moose::Util::TypeConstraints;
 
 use namespace::clean -except => 'meta';
 
-with qw(
-    MooseX::Declare::Syntax::KeywordHandling
-);
+with 'MooseX::Declare::Syntax::MethodDeclaration';
 
-has prototype_beginning => (
+has modifier_type => (
     is          => 'rw',
-    isa         => 'Str',
+    isa         => enum(undef, qw( around after before override augment )),
+    required    => 1,
 );
 
-sub parse {
-    my ($self, $ctx) = @_;
-
-    $ctx->skip_declarator;
-    local $Carp::Internal{'Devel::Declare'} = 1;
-
-    my $name = $ctx->strip_name;
-    return unless defined $name;
-
-    my $proto = $ctx->strip_proto || '';
-
-    $proto = join(', ',
-        $self->prototype_beginning || (),
-        length($proto) ? $proto : (),
-    );
-
-    my $method = MooseX::Method::Signatures::Meta::Method->wrap(
-        signature    => qq{(${proto})},
-        package_name => $ctx->get_curstash_name,
-        name         => $name,
-    );
-
-    $ctx->inject_if_block( $ctx->scope_injector_call() . $method->injectable_code );
-
-    my $modifier_name = $ctx->declarator;
-    $ctx->shadow(sub (&) {
-        my $class = caller();
-        $method->_set_actual_body(shift);
-        Moose::Util::add_method_modifier($class, $modifier_name, [$name => $method->body]);
-    });
+sub register_method_declaration {
+    my ($self, $class, $method) = @_;
+    return Moose::Util::add_method_modifier($class, $self->modifier_type, [$method->name, $method->body]);
 }
 
 1;
