f2e8c36f27b70b02802667b27dde46173258818c
Florian Ragwitz <rafl@debian.org>
2009-08-15 11:03:14 +0200

    Let perl handle the error reporting until we implement with BAREWORD and with (BAREWORD, BAREWORD).

--- lib/MooseX/Declare/Syntax/Keyword/With.pm
+++ lib/MooseX/Declare/Syntax/Keyword/With.pm
@@ -16,12 +16,6 @@ sub parse {
     my ($self, $ctx) = @_;
 
     $ctx->skip_declarator;
-    $ctx->skipspace;
-
-    my $next_char = $ctx->peek_next_char;
-    if ($next_char =~ /^[\w(:]$/) {
-        confess "Declarative 'with' not implemented yet. Use the normal Moose 'with' syntax.";
-    }
 
     my $pkg = outer_stack_peek $ctx->caller_file;
     $ctx->shadow(sub {
