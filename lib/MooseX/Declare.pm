fdea428a5798f6ccb34d1d6f1ae8152158d1f847
Florian Ragwitz <rafl@debian.org>
2010-02-04 02:56:20 +0100

    Version 0.33.

--- lib/MooseX/Declare.pm
+++ lib/MooseX/Declare.pm
@@ -9,7 +9,7 @@ use aliased 'MooseX::Declare::Syntax::Keyword::Namespace',  'NamespaceKeyword';
 
 use namespace::clean;
 
-our $VERSION = '0.32';
+our $VERSION = '0.33';
 
 sub import {
     my ($class, %args) = @_;
